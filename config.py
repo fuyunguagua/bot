9474228e33a2f06a55a95defce5ff186224c1088
fuyunguagua <425006762@qq.com>
2018-01-08 22:41:38 +0800

    afjadij9

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import MySQLdb
 class DB:
     HOST = '10.0.0.2'
     USER = 'root'
-    PASS = 'abcd1234'
+    PASS = 'abcd123466666666'
     NAME = 'BOTDATA'
 
 db = MySQLdb.connect(DB.HOST, DB.USER, DB.PASS, DB.NAME)
